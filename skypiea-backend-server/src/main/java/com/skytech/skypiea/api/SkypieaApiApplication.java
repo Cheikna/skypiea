caf67868bb8b41e4495425444a87d29ff2210bc3
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-10-11 17:07:04 +0200

    Project initialization with Spring Boot and Angular
    - Creation of the different modules for the backend server
    - Creation of the frontend part with angular

---
+++ skypiea-backend-server/src/main/java/com/skytech/skypiea/api/SkypieaApiApplication.java
@@ -0,0 +1,13 @@
+package com.skytech.skypiea.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypieaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkypieaApiApplication.class, args);
+	}
+
+}
