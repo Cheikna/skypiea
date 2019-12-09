262338bc83d728734aa6eedcb02b9e4ed527db44
Tristan <tristan.dasilvasousa@gmail.com>
2019-12-09 11:36:56 +0100

    entity TemperatureController

---
+++ skypiea-commons/src/main/java/com/skytech/skypiea/commons/entity/TemperatureController.java
@@ -0,0 +1,107 @@
+package com.skytech.skypiea.commons.entity;
+
+
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+
+@Entity
+@Table(name="TemperatureController")
+public class TemperatureController{
+	
+	//private static Logger log = LoggerFactory.getLogger(Character.class);
+	
+	@JoinColumn(name = "id")
+    private NonMedicalConnectedObject nonMedicalConnectedObject;
+	
+	@Column(name="threshold")
+	private int threshold;	
+	
+	@Column(name="temperature_Max")
+	private int temperature_Max;
+
+	@Column(name="temperature_Min")
+	private int temperature_Min;
+	
+	@Column(name="temperature_Settled ")
+	private int temperature_Settled;
+	
+	@Column(name="is_Healthing_Connected")
+	private boolean is_Healthing_Connected;
+	
+	@Column(name="is_Air_Conditionner_Connected")
+	private boolean is_Air_Conditionner_Connected;
+	
+	@JoinColumn(name = "id_Room")
+    private Room room;
+
+	public NonMedicalConnectedObject getNon_medical_connected_object() {
+		return nonMedicalConnectedObject;
+	}
+
+	public void setNon_medical_connected_object(NonMedicalConnectedObject nonMedicalConnectedObject) {
+		this.nonMedicalConnectedObject = nonMedicalConnectedObject;
+	}
+
+	public int getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(int threshold) {
+		this.threshold = threshold;
+	}
+
+	public int getTemperature_Max() {
+		return temperature_Max;
+	}
+
+	public void setTemperature_Max(int temperature_Max) {
+		this.temperature_Max = temperature_Max;
+	}
+
+	public int getTemperature_Min() {
+		return temperature_Min;
+	}
+
+	public void setTemperature_Min(int temperature_Min) {
+		this.temperature_Min = temperature_Min;
+	}
+
+	public int getTemperature_Settled() {
+		return temperature_Settled;
+	}
+
+	public void setTemperature_Settled(int temperature_Settled) {
+		this.temperature_Settled = temperature_Settled;
+	}
+
+	public boolean isIs_Healthing_Connected() {
+		return is_Healthing_Connected;
+	}
+
+	public void setIs_Healthing_Connected(boolean is_Healthing_Connected) {
+		this.is_Healthing_Connected = is_Healthing_Connected;
+	}
+
+	public boolean isIs_Air_Conditionner_Connected() {
+		return is_Air_Conditionner_Connected;
+	}
+
+	public void setIs_Air_Conditionner_Connected(boolean is_Air_Conditionner_Connected) {
+		this.is_Air_Conditionner_Connected = is_Air_Conditionner_Connected;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+}
+	
+	
