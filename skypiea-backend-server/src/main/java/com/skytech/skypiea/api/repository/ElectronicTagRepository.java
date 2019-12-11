ed8fb474c03a478bb737fc8ea8815d35517e9de8
Guillaume TAOUDIAT <taoudiat.guillaume@gmail.com>
2019-12-11 14:08:55 +0100

    NPM

--- skypiea-backend-server/src/main/java/com/skytech/skypiea/api/repository/ElectronicTagRepository.java
+++ skypiea-backend-server/src/main/java/com/skytech/skypiea/api/repository/ElectronicTagRepository.java
@@ -3,7 +3,7 @@ package com.skytech.skypiea.api.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.skytech.skypiea.commons.entity.ETag;
+import com.skytech.skypiea.commons.entity.ElectronicTag;
 
 /*@Repository
 public interface ElectronicTagRepository extends JpaRepository<ElectronicTag, Long>{
--- skypiea-backend-server/src/main/java/com/skytech/skypiea/api/service/ElectronicTagService.java
+++ skypiea-backend-server/src/main/java/com/skytech/skypiea/api/service/ElectronicTagService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //import com.skytech.skypiea.api.repository.ElectronicTagRepository;
-import com.skytech.skypiea.commons.entity.ETag;
+import com.skytech.skypiea.commons.entity.ElectronicTag;
 
 @Service
 public class ElectronicTagService {
