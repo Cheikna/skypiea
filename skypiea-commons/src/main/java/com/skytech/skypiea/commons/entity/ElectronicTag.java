c1b1927ac9f77172d8b43e317e76434c617812f4
Guillaume TAOUDIAT <taoudiat.guillaume@gmail.com>
2019-12-11 14:08:55 +0100

    NPM

--- skypiea-commons/src/main/java/com/skytech/skypiea/commons/entity/ElectronicTag.java
+++ skypiea-commons/src/main/java/com/skytech/skypiea/commons/entity/ElectronicTag.java
@@ -1,12 +1,12 @@
 package com.skytech.skypiea.commons.entity;
 import java.util.ArrayList;
 
-public class ETag {
+public class ElectronicTag {
 	private long id;
 	private int residentID;
 	private ArrayList<HistoryMoving> historyMoving;
 	
-	public ETag(long id, int residentID, int idPosX, int idPosY, ArrayList<HistoryMoving> historyMoving) {
+	public ElectronicTag(long id, int residentID, int idPosX, int idPosY, ArrayList<HistoryMoving> historyMoving) {
 		this.id = id;
 		this.residentID = residentID;
 		this.historyMoving = historyMoving;
