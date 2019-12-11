b76d740d5ae5fd0e8055eff03560010700cab1e6
Guillaume TAOUDIAT <taoudiat.guillaume@gmail.com>
2019-12-11 15:19:41 +0100

    delete bad lines from etag services

--- skypiea-backend-server/src/main/java/com/skytech/skypiea/api/repository/ElectronicTagRepository.java
+++ skypiea-backend-server/src/main/java/com/skytech/skypiea/api/repository/ElectronicTagRepository.java
@@ -3,7 +3,6 @@ package com.skytech.skypiea.api.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.skytech.skypiea.commons.entity.ElectronicTag;
 
 /*@Repository
 public interface ElectronicTagRepository extends JpaRepository<ElectronicTag, Long>{
