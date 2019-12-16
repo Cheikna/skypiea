ae103887cc640bf1702e046aaa0c8306a59384fd
Faizaan Meghani <faizaan.meghani@etu.u-pec.fr>
2019-12-16 19:14:13 +0100

    Creation of components for the failure tab

--- skypiea-web/src/app/app-routing.module.ts
+++ skypiea-web/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { PersonnalFormComponent } from './components/personnal-form/personnal-fo
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+import { FailureComponent } from './components/failure/failure.component';
 
 
 const routes: Routes = [
@@ -79,6 +80,14 @@ const routes: Routes = [
     canActivate: [AuthenticationGuard], 
     data: { 
       userType: 'NOT_CONNECTED'
+    }
+  },
+  {
+    path: 'failure', 
+    component: FailureComponent, 
+    canActivate: [AuthenticationGuard], 
+    data: { 
+      userType: 'STAFF'
     }
   },
   {
