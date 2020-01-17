d15d74268b9a66333a227c8aa5f083c7bc4dd89d
Faizaan Meghani <faizaan.meghani@etu.u-pec.fr>
2020-01-17 14:06:43 +0100

    Creation of the task which will call the algorithm

--- skypiea-batch/src/main/java/com/skytech/skypiea/batch/service/TimerTaskLauncherService.java
+++ skypiea-batch/src/main/java/com/skytech/skypiea/batch/service/TimerTaskLauncherService.java
@@ -5,19 +5,20 @@ import java.util.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skytech.skypiea.batch.timer.TasksRunner;
+import com.skytech.skypiea.batch.tasks.RoomObjectsCheckerTask;
 
 @Service
 public class TimerTaskLauncherService {
 	
-	private final Long breakTime = 5000L;
+	private final Long roomCheckerFrequency = 10000L;
 	
 	@Autowired	
-	private TasksRunner tasksRunner;
+	private RoomObjectsCheckerTask roomObjectsCheckerTask;
+	//TODO Add other tasks like notification checker, 
 	
 	public void start() {		
 		Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(tasksRunner, 0L, breakTime);
+        timer.scheduleAtFixedRate(roomObjectsCheckerTask, 0L, roomCheckerFrequency);
 	}
 
 }
