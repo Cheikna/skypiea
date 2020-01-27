fb097ae0f31c670d4afafbc6cd24ac5d7ca58f2a
Cheikna DANSOKO <cheikna98@hotmail.fr>
2020-01-27 14:31:41 +0100

    Creation of some Utils class
    Creation of util class for the JSON serialization and

---
+++ skypiea-injector/src/test/java/com/skytech/skypiea/injector/util/ScannerUtilTest.java
@@ -0,0 +1,76 @@
+package com.skytech.skypiea.injector.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class ScannerUtilTest {
+	
+	private Scanner sc;
+	private Predicate<String> nullPredicate = null;
+	private Predicate<String> stringSizePredicate = (str) -> {
+		return str != null && str.length() >= 3;
+	};
+	
+	@After
+	public void close() {
+		if(sc != null) {
+			sc.close();
+		}
+	}
+
+	@Test
+	public void testAskForStringNullPredicate() {
+		String[] inputs = {"testTEST"};
+		setScannerInputs(inputs);
+		assertNull(ScannerUtil.askForString(sc, "question", nullPredicate));
+		
+	}
+	
+	@Test
+	public void testAskForStringPredicateFirstNotValidated() {
+		String correct = "cotrdc";
+		String[] inputs = {"te", "co", correct };
+		setScannerInputs(inputs);
+		assertEquals(correct, ScannerUtil.askForString(sc, "question", stringSizePredicate));
+	}
+	
+	@Test
+	public void testAskForStringPredicateValidated() {
+		String answer = "testTEST";
+		String[] inputs = {answer};
+		setScannerInputs(inputs);
+		assertEquals(answer, ScannerUtil.askForString(sc, "question", stringSizePredicate));
+	}
+	
+	@Test
+	public void testIsNumberPredicate() {
+		assertTrue(ScannerUtil.isNumber.test("5"));
+		assertTrue(ScannerUtil.isNumber.test("39"));
+		assertFalse(ScannerUtil.isNumber.test("3f9"));
+	}
+
+	@Test
+	public void testWantToQuit() {
+		assertTrue(ScannerUtil.wantToQuit("q"));
+		assertTrue(ScannerUtil.wantToQuit("Q"));
+		assertFalse(ScannerUtil.wantToQuit("Qe"));
+		assertFalse(ScannerUtil.wantToQuit("qQ"));
+	}
+	
+	private void setScannerInputs(String[] inputsArray) {
+		String newInput = System.getProperty("line.separator");
+		String inputs = String.join(newInput, inputsArray);
+		System.setIn(new ByteArrayInputStream(inputs.getBytes()));
+		sc = new Scanner(System.in);
+	}
+
+}
