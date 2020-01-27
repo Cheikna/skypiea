6cf795d7d9fa6a019d1a746ee7c1d4f84551ff33
Cheikna DANSOKO <cheikna98@hotmail.fr>
2020-01-27 16:22:23 +0100

    Adding task to retrieve data from database and some classes to store data in cache

--- skypiea-batch/src/main/java/com/skytech/skypiea/batch/algorithm/abstracts/NonMedicalConnectedObjectAlgorithm.java
+++ skypiea-batch/src/main/java/com/skytech/skypiea/batch/algorithm/abstracts/NonMedicalConnectedObjectAlgorithm.java
@@ -3,7 +3,7 @@ package com.skytech.skypiea.batch.algorithm.abstracts;
 import com.skytech.skypiea.commons.entity.NonMedicalConnectedObject;
 import com.skytech.skypiea.commons.enumeration.State;
 
-public abstract class NonMedicalConnectedObjectAlgorithm {
+public abstract class NonMedicalConnectedObjectAlgorithm {	
 	
 	public abstract State check(NonMedicalConnectedObject nonMedicalConnectedObject);
 }
--- skypiea-batch/src/main/java/com/skytech/skypiea/batch/service/TimerTaskLauncherService.java
+++ skypiea-batch/src/main/java/com/skytech/skypiea/batch/service/TimerTaskLauncherService.java
@@ -5,20 +5,32 @@ import java.util.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skytech.skypiea.batch.tasks.RoomObjectsCheckerTask;
+import com.skytech.skypiea.batch.socket.server.BatchMessageReceiverSocket;
+import com.skytech.skypiea.batch.task.implementation.MemoryCacheCheckerTask;
+import com.skytech.skypiea.batch.task.implementation.RoomStateCheckerTask;
 
 @Service
 public class TimerTaskLauncherService {
 	
+	private final Long roomCheckerDelay = 5000L;
 	private final Long roomCheckerFrequency = 10000L;
+	private final Long memoryCacheCheckerFrequency = 10000L;
+	
+	@Autowired
+	private BatchMessageReceiverSocket batchServerSocket;
 	
 	@Autowired	
-	private RoomObjectsCheckerTask roomObjectsCheckerTask;
-	//TODO Add other tasks like notification checker, 
+	private RoomStateCheckerTask roomObjectsCheckerTask;
+	
+	@Autowired
+	private MemoryCacheCheckerTask memoryCacheCheckerTask;
 	
 	public void start() {		
 		Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(roomObjectsCheckerTask, 0L, roomCheckerFrequency);
+        timer.schedule(batchServerSocket, 0L);
+        timer.scheduleAtFixedRate(memoryCacheCheckerTask, 0L, memoryCacheCheckerFrequency);
+        // Add a delay in order to let the socket start
+        timer.scheduleAtFixedRate(roomObjectsCheckerTask, roomCheckerDelay, roomCheckerFrequency);
 	}
 
 }
