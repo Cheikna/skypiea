82da2bfe3963c5f60dc02988c746d83899719c34
Cheikna DANSOKO <cheikna98@hotmail.fr>
2020-01-31 09:40:27 +0100

    Modifying the message receiver runnable

--- skypiea-injector/src/main/java/com/skytech/skypiea/injector/util/ScannerUtil.java
+++ skypiea-injector/src/main/java/com/skytech/skypiea/injector/util/ScannerUtil.java
@@ -16,31 +16,39 @@ public class ScannerUtil {
 	 * @param sc : the scanner to read the user answer
 	 * @param question : the indication to display to the user
 	 * @param predicate : the predicate to check on the user answer
+	 * @param defaultValue : the default value if the scanner read nothing or an empty string
 	 * @return
 	 */
-	public static String askForString(Scanner sc, String question, Predicate<String> predicate) {
+	public static String askForString(Scanner sc, String question, Predicate<String> predicate, String defaultValue) {
 		boolean isAnswerCorrect = false;
 		String answer = null;
-		if(predicate != null) {
-			do {
-				// Display the question to the user
-				System.out.print(question);
-				
-				// Wait for a user input and the enter key
-				answer = sc.nextLine();
-				
-				// Check if the entered answered is not empty and not null
-				isAnswerCorrect = (!GenericValidator.isBlankOrNull(answer));
+		do {
+			// Display the question to the user
+			System.out.print(question);
+			
+			// Wait for a user input and the enter key
+			answer = sc.nextLine();
+			
+			// Check if the entered answered is not empty and not null
+			if(GenericValidator.isBlankOrNull(answer)) {
+				answer = defaultValue;
+			}
 
-				// Check if the entered answer is accepted by the predicate
-				isAnswerCorrect = (isAnswerCorrect && predicate.test(answer));
+			// Check if the entered answer is accepted by the predicate
+			if(predicate != null) {
+				isAnswerCorrect = predicate.test(answer);
+			}
 
-				if(!isAnswerCorrect) {
-					System.out.println("/!\\ The answer is incorrect /!\\");
-				}
-			} while(!isAnswerCorrect);
-		}
+			if(!isAnswerCorrect) {
+				System.out.println("/!\\ The answer is incorrect /!\\");
+			}
+		} while(!isAnswerCorrect);
+		
 		return answer;		
+	}
+	
+	public static String askForString(Scanner sc, String question, Predicate<String> predicate) {
+		return askForString(sc, question, predicate, "default");
 	}
 	
 	public static Predicate<String> isNumber = (str) -> {
