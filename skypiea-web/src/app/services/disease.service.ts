8a5f0f0bc0202af6923a3cf829965dc3cdd0364e
Sakana KANESWARAN <sakana.kaneswaran@etu.u-pec.fr>
2020-02-10 16:34:31 +0100

    merge with master

--- skypiea-web/src/app/services/disease.service.ts
+++ skypiea-web/src/app/services/disease.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RestService } from './rest.service';
-import { DiseaseType } from '../enums/disease.enum';
-import { Disease } from '../models/disease.model';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { DiseaseFormComponent } from '../sub-components/disease-form/disease-form.component';
 
