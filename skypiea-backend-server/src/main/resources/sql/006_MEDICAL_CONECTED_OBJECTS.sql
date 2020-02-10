474e99b37d715c22e57e133b01ffb787c685c23b
Andrei Petrov <andrei.petrov@etu.u-pec.fr>
2020-02-10 10:08:19 +0100

    mock data

--- skypiea-backend-server/src/main/resources/sql/006_MEDICAL_CONECTED_OBJECTS.sql
+++ skypiea-backend-server/src/main/resources/sql/006_MEDICAL_CONECTED_OBJECTS.sql
@@ -15,7 +15,7 @@ MEDICAL_CONNECTED_OBJECT_ID integer REFERENCES MEDICAL_CONNECTED_OBJECT(ID)
 );
 
 insert into CONNECTED_OBJECT (ID, VERSION, BRAND, IP_ADDRESS, MAC_ADDRESS, LAST_MEASUREMENT_DATE, LAST_PARAMETER_MODIFICATION_DATE, STATUS, STATE)
-values (nextval(pg_get_serial_sequence('connected_object', 'id')), 1, 'Somfy', '72.135.238.212', '80:25:aa:2b:6e:e6', '2019-12-16 09:00:00.000', '2019-12-16 09:00:00.000', 'ON', 'OPERATIONAL');
+values (nextval(pg_get_serial_sequence('connected_object', 'id')), 1, 'Somfy', '192.168.6.1', '80:25:aa:2b:6e:e6', '2019-12-16 09:00:00.000', '2019-12-16 09:00:00.000', 'ON', 'OPERATIONAL');
 
 insert into MEDICAL_CONNECTED_OBJECT values (currval(pg_get_serial_sequence('connected_object', 'id')),(select ID from SKYPIEA_USER where first_name = 'Leo'));
 
--- skypiea-injector/src/main/java/com/skytech/skypiea/injector/mock/healthcare/HealthControlMock.java
+++ skypiea-injector/src/main/java/com/skytech/skypiea/injector/mock/healthcare/HealthControlMock.java
@@ -1,14 +1,48 @@
 package com.skytech.skypiea.injector.mock.healthcare;
 
+import com.skytech.skypiea.commons.enumeration.MessageSender;
 import com.skytech.skypiea.injector.mock.abstracts.Mock;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class HealthControlMock extends Mock {
+
+    private static Logger log = LoggerFactory.getLogger(HealthControlMock.class);
+
+    private final String FILE_PATH = "health-control/mock-health-rate.txt";
+
     @Override
     public void start(Scanner sc) {
 
     }
+
+    Consumer<String> formatMockTextFileAndSend = (line) -> {
+        // Skip the empty line and the ones which starts with '--'
+        if(line != null && (line.trim().length() > 0) && !line.startsWith("--")) {
+            String[] splitted = line.split(";");
+            if(splitted.length >= 3) {
+                String macAddress = splitted[0];
+                String valueStr = splitted[1];
+                String sleepStr = splitted[2];
+                Message message = new Message(macAddress, MessageSender.NON_MEDICAL_CONNECTED_OBJECT, valueStr, "");
+                clientSocket.sendMessage(message);
+
+                Long sleep = NumberUtils.toLong(sleepStr, 1) * 1000;
+                try {
+                    Thread.sleep(sleep);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                log.info("One of the values is missing : ID;VALUE;SLEEP");
+            }
+        }
+    };
 
     @Override
     public String getMockName() {
