66acff27878b498e0e4091e3f2415d53c2726f6e
Cheikna DANSOKO <cheikna98@hotmail.fr>
2020-02-10 19:29:48 +0100

    Adding the possibilityto select a floor (=un étage) and displaying an icon which represents the state of the room on the page which gives the list of rooms

--- skypiea-web/src/app/sub-components/room-summary/room-summary.component.ts
+++ skypiea-web/src/app/sub-components/room-summary/room-summary.component.ts
@@ -4,6 +4,7 @@ import { Resident } from 'src/app/models/resident.model';
 import { nonMedicalObjectIcons } from 'src/app/util/nonMedicalObjectIcons.util';
 import { Router } from '@angular/router';
 import { getNonMedicalObjectTypeName } from 'src/app/enums/nonMedicalObjectType.enum';
+import { stateInfo } from 'src/app/enums/state.enum';
 
 @Component({
   selector: 'app-room-summary',
@@ -18,6 +19,7 @@ export class RoomSummaryComponent implements OnInit {
   // To import
   nonMedicalObjectIcons = nonMedicalObjectIcons;
   typeName = (type) => getNonMedicalObjectTypeName(type);
+  stateInfo = stateInfo;
 
   constructor(private router: Router) { }
 
