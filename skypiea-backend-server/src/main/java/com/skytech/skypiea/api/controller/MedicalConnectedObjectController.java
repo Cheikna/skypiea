2b97eef62bf4e9be56246d0e4297ada803f3fb63
Andrei Petrov <andrei.petrov@etu.u-pec.fr>
2020-02-10 17:30:25 +0100

    implement method to save health_control_history in database

--- skypiea-backend-server/src/main/java/com/skytech/skypiea/api/controller/MedicalConnectedObjectController.java
+++ skypiea-backend-server/src/main/java/com/skytech/skypiea/api/controller/MedicalConnectedObjectController.java
@@ -1,12 +1,10 @@
 package com.skytech.skypiea.api.controller;
 
 import com.skytech.skypiea.api.service.MedicalConnectedObjectService;
+import com.skytech.skypiea.commons.entity.HealthControlHistory;
 import com.skytech.skypiea.commons.entity.MedicalConnectedObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,11 @@ public class MedicalConnectedObjectController {
     @GetMapping("")
     public List<MedicalConnectedObject> getMedicalConnectedObject() {
         return medicalConnectedObjectService.findAll();
+    }
+
+    @PostMapping("/save/{ipAddress}")
+    public MedicalConnectedObject saveHistory(@PathVariable String ipAddress, HealthControlHistory healthControlHistory){
+        return this.medicalConnectedObjectService.saveHistory(ipAddress,healthControlHistory);
     }
 
 }
--- skypiea-batch/src/main/java/com/skytech/skypiea/batch/service/HealthSurveillanceService.java
+++ skypiea-batch/src/main/java/com/skytech/skypiea/batch/service/HealthSurveillanceService.java
@@ -1,12 +1,25 @@
 package com.skytech.skypiea.batch.service;
 
+import com.skytech.skypiea.api.service.MedicalConnectedObjectService;
+import com.skytech.skypiea.commons.entity.HealthControlHistory;
 import com.skytech.skypiea.commons.message.Message;
+import com.skytech.skypiea.commons.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthSurveillanceService  implements IMessageProcessor {
+
+    @Autowired
+    private MedicalConnectedObjectService medicalConnectedObjectService;
     @Override
     public void processObjectMessage(Message message) {
-        System.out.println("--------> " + message);
+        String ipAddress = message.getIpAddress();
+        String value = message.getValue1();
+        HealthControlHistory healthControlHistory = new HealthControlHistory();
+        healthControlHistory.setSavingDate(DateUtil.getCurrentTimestamp());
+        healthControlHistory.setValue(Float.valueOf(value));
+        this.medicalConnectedObjectService.saveHistory(ipAddress,healthControlHistory);
     }
+
 }
