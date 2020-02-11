8d1ab5547f5a29dff4f4d227236d3a717b05ba24
Cheikna DANSOKO <cheikna98@hotmail.fr>
2020-02-11 20:46:48 +0100

    Modifying OnToMany relationship + Adding the "CODE" attribute on the door connected object
    Modification of a OneToMany relationship so that the objectSetting object can have a reference of its parent object which is a nonMedicalConnected Object

--- skypiea-commons/src/main/java/com/skytech/skypiea/commons/entity/DoorSensor.java
+++ skypiea-commons/src/main/java/com/skytech/skypiea/commons/entity/DoorSensor.java
@@ -16,14 +16,19 @@ public class DoorSensor extends ObjectSetting  {
 	@Column(name="IS_DOOR_OPENED")
 	private Boolean isDoorOpened;
 	
+	@Column(name="CODE")
+	private String code;
+	
 	public DoorSensor() {
 		super();
 	}	
 
-	public DoorSensor(Long id, Long version, Timestamp savingDate, boolean isDoorLocked, boolean isDoorOpened) {
+	public DoorSensor(Long id, Long version, Timestamp savingDate, boolean isDoorLocked, boolean isDoorOpened,
+			String code) {
 		super(id, version, savingDate);
 		this.isDoorLocked = isDoorLocked;
 		this.isDoorOpened = isDoorOpened;
+		this.code = code;
 	}
 
 	public boolean isDoorLocked() {
@@ -40,5 +45,13 @@ public class DoorSensor extends ObjectSetting  {
 
 	public void setDoorOpened(boolean isDoorOpened) {
 		this.isDoorOpened = isDoorOpened;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
 	}	
 }
