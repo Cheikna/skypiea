444b849e53dd04c51139f94b404f92ba5a0a8449
Sakana KANESWARAN <sakana.kaneswaran@etu.u-pec.fr>
2020-02-24 14:24:40 +0100

    file modified

--- skypiea-web/src/app/sub-components/disease-form/disease-form.component.ts
+++ skypiea-web/src/app/sub-components/disease-form/disease-form.component.ts
@@ -28,25 +28,6 @@ export class DiseaseFormComponent implements OnInit {
   }
 
   create(){
-    /*console.log(this.diseaseService.getTypeOfDisease());
-    this.newDisease.typeOfDisease = this.diseaseService.getTypeOfDisease();
-
-    console.log(JSON.stringify(this.newDisease));
-    console.log("test : " + this.newDisease.typeOfDisease);
-    
-    this.diseaseService.create(this.newDisease).subscribe(
-      (data) => {
-        if(data){
-          console.log("disease created");
-            this.toastService.displayToast(ToastType.SUCCESS, 'Successful connection', true, 'Please wait, the page will reload');
-        } else {
-          this.toastService.displayToast(ToastType.ERROR, 'Authentication failed !', true, 
-          'Your username or your password is incorrect.', 7000);
-        }        
-      },
-      (error) => this.toastService.displayToast(ToastType.ERROR, 'An error occured !', true, JSON.stringify(error)));*/
-      console.log(this.newDisease.treatment);
-      console.log(this.newDisease.duration);
       this.diseaseService.setTreatment(this.newDisease.treatment);
       this.diseaseService.setDuration(this.newDisease.duration);
   }
