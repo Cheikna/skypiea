ffd36e6dc42064cf2345d116a45ce53ef911ef1f
Guillaume TAOUDIAT <taoudiat.guillaume@gmail.com>
2020-02-24 14:28:48 +0100

    adding searchbar, pagination for tracklist component

--- skypiea-web/src/app/components/tracklist-characters/tracklist-characters.component.ts
+++ skypiea-web/src/app/components/tracklist-characters/tracklist-characters.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { ElectronicTagService } from 'src/app/services/electronictag.service';
 import { ElectronicTag } from 'src/app/models/electronictag.model';
 import { Resident } from 'src/app/models/resident.model';
 import { ResidentService } from 'src/app/services/resident.service';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 
 @Component({
@@ -11,26 +12,34 @@ import { ResidentService } from 'src/app/services/resident.service';
   templateUrl: './tracklist-characters.component.html',
   styleUrls: ['./tracklist-characters.component.scss']
 })
-export class TracklistCharactersComponent implements OnInit {
-  residents: Array<Resident>;
+export class TracklistCharactersComponent implements OnInit, AfterViewInit {
   tag: ElectronicTag;
-  newResident: Resident;
+  public dataSource = new MatTableDataSource<Resident>();
+  public displayedColumns = ['lastName', 'firstName', 'select'];
+
+  @ViewChild(MatPaginator, {static:false}) paginator: MatPaginator;
 
   constructor(private residentService: ResidentService, private router: Router, private electronicTagService: ElectronicTagService) { 
-    this.residents = new Array<Resident>();
-    this.newResident = new Resident();
   }
 
   ngOnInit() {
     this.findAll();
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
   findAll(){
     this.residentService.findAll().subscribe((data: Array<Resident>)  => {
-      this.residents = data;
+      this.dataSource.data = data;
     });
-
   }
+
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
+
 
   select(id: number){
     this.router.navigate(['/tracking/'+id])
