bb75149229dfc9a0b6e38cef1891423ad7a4edec
Guillaume TAOUDIAT <taoudiat.guillaume@gmail.com>
2020-02-24 14:28:12 +0100

    Add pick date for tracking component + design update

--- skypiea-web/src/app/components/tracking/tracking.component.ts
+++ skypiea-web/src/app/components/tracking/tracking.component.ts
@@ -3,6 +3,7 @@ import { ElectronicTag } from 'src/app/models/electronictag.model';
 import { ElectronicTagService } from 'src/app/services/electronictag.service';
 import { ActivatedRoute } from '@angular/router';
 import { HistoryMoving } from 'src/app/models/historyMoving.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-tracking',
@@ -12,25 +13,59 @@ import { HistoryMoving } from 'src/app/models/historyMoving.model';
 export class TrackingComponent implements OnInit {
   //create array of history moving : movings
   movings: Array<HistoryMoving>;
+  allMovings: Array<HistoryMoving>;
   //add electronictag object
   electronicTag: ElectronicTag;
+  dateBegin = new FormControl(new Date());
   //declare service inside constructor
   constructor(private route: ActivatedRoute, private electronicTagService: ElectronicTagService) { 
     this.electronicTagService = electronicTagService;
+    this.allMovings = new Array<HistoryMoving>();
+    this.movings = new Array<HistoryMoving>();
   }
 
   ngOnInit() {
     //Get ID from url (Should not be null)
     let id = this.route.snapshot.paramMap.get('id');
     //Call service method from constructor to get ElectronicTag of a resident
-    this.electronicTagService.getResidentHistoryMoving(parseInt(id)).subscribe((data: ElectronicTag) => {
-      this.movings = data.historyMoving;
+    this.electronicTagService.getResidentHistoryMoving(parseInt(id)).subscribe((data: ElectronicTag) => {      
+      this.allMovings = data.historyMoving;
+      if(this.allMovings){
+        this.allMovings.sort((obj1, obj2) => {
+          if(obj1.time != null && obj2.time == null){
+            return 1;
+          } else if(obj1.time == null && obj2.time != null){
+            return -1;
+          } else if(obj1.time == null || obj2.time == null){
+            return -1;
+          }
+          return (new Date(obj2.time).getTime()) - (new Date(obj1.time).getTime());
+        });
+      }
+      Object.assign(this.movings, this.allMovings);
       /*movings.forEach(move => {
         console.log(move.posX + " " + move.posY);
       });*/
     });
     // Display data to console
     //console.log(JSON.stringify(id));
+  }
+
+  updatePositionsToDisplay(){
+    this.movings = this.allMovings.filter((position) => {
+      const dateBeginValue: Date = this.dateBegin.value;
+      const time: number = dateBeginValue.getTime();
+      const positionDate: Date = new Date(position.time);
+      if(positionDate && positionDate.getTime() > time){
+        return true;
+      }
+      return false;
+    });
+  }
+
+  resetPositionsToDisplay(){
+    this.dateBegin.setValue(new Date());
+    Object.assign(this.movings, this.allMovings);
   }
 
 }
