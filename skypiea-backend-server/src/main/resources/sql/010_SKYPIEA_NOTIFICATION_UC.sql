ae8ed4d37374b3d399cad7a2333563f66e749d66
Cheikna DANSOKO <cheikna98@hotmail.fr>
2020-04-27 12:20:10 +0200

    Update of SQL scripts and staff mailbox components (html & ts)
    - Adding new template in the SQL scripts in order to have a message for the different state of an object (danger, warning and broken)
    - Modifying the staff components in order to display the objects which match the message object (if in the object we enter missing object then only the objects of the room which have a missing state will be displayed)
    - Adding the possibility to clear the message object input

--- skypiea-backend-server/src/main/resources/sql/010_SKYPIEA_NOTIFICATION_UC.sql
+++ skypiea-backend-server/src/main/resources/sql/010_SKYPIEA_NOTIFICATION_UC.sql
@@ -25,19 +25,27 @@ INSERT INTO NOTIFICATION_TEMPLATE(MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('EHPAD 2
 INSERT INTO NOTIFICATION_TEMPLATE(MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('Heatwave', 'Because of the heatwave, we recommand you to drink a lot and only water &#1F60F;<br>Let your shutters closed and do not hesitate to take a shower !');
 INSERT INTO NOTIFICATION_TEMPLATE(MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('Pharmaceutical drugs recalled', 'Do not forget to take your drugs');
 INSERT INTO NOTIFICATION_TEMPLATE(MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('Tips to fight the flu', 'The flu is coming - Tips against flu<br>
----- Wash your hands ----<br>
+---- Wash your hands ----
 Good hygiene is one of the best ways to help prevent a cold or flu from spreading. Wash your hands regularly with soap and water.<br><br>
----- Cover coughs and sneezes ----<br>
+---- Cover coughs and sneezes ----
 Cover your mouth and nose when coughing or sneezing. Preferably with your elbow rather than your hand.<br><br>
----- Bin your tissues ----<br>
+---- Bin your tissues ----
 Throw disposable tissues in the bin immediately after using them.<br><br>
----- Avoid sharing  ----<br>
+---- Avoid sharing  ----
 Try not to share cups, plates, cutlery and towels with other people, as this could spread germs.<br><br>
----- Don’t rely on antibiotics ----<br>
+---- Don’t rely on antibiotics ----
 Antibiotics won’t cure a cold or flu as these illnesses are caused by viruses.<br>
 Antibiotics only work for bacterial infections. Check if your cold and flu medication will help relieve symptoms on healthdirect’s medicine directory.<br><br>
----- Know the symptoms of a cold versus flu ----<br>
+---- Know the symptoms of a cold versus flu ----
 A cold is not life-threatening and although uncomfortable does not require visit to a GP. A flu can escalate and may require medical attention.<br><br>
 from https://www.healthdirect.gov.au/10-tips-to-fight-the-flu-infographic');
-INSERT INTO NOTIFICATION_TEMPLATE(NOTIFICATION_OBJECT,MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('ABOUT_OBJECT', 'Information about one of your object : ','The mentionned object need a technical control <br>
+INSERT INTO NOTIFICATION_TEMPLATE(NOTIFICATION_OBJECT,MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('ABOUT_OBJECT', 'Technical control','The mentionned object need a technical control. <br>
 One of our technicians will check this object within the next 24 hours');
+INSERT INTO NOTIFICATION_TEMPLATE(NOTIFICATION_OBJECT,MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('ABOUT_OBJECT', 'Information about a broken object','The mentionned object is broken. <br>
+One of our technicians will check this object within the next 30 minutes');
+INSERT INTO NOTIFICATION_TEMPLATE(NOTIFICATION_OBJECT,MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('ABOUT_OBJECT', 'Information about a missing object','The mentionned object is missing. <br>
+Perhaps, the object has been unplugged or is switched off. One of our technicians will check this object within the next 30 minutes');
+INSERT INTO NOTIFICATION_TEMPLATE(NOTIFICATION_OBJECT,MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('ABOUT_OBJECT', 'Information about a warning object','The mentionned object is in warning. <br>
+Please be careful otherwise it will end with a danger !');
+INSERT INTO NOTIFICATION_TEMPLATE(NOTIFICATION_OBJECT,MESSAGE_OBJECT, MESSAGE_BODY) VALUES ('ABOUT_OBJECT', 'Information about a danger object','The mentionned object is in danger. <br>
+The object raises a danger. Please step out of your room ! A tema will intervenes in the next minutes.');
--- skypiea-web/src/app/components/mail-box-staff/mail-box-staff.component.ts
+++ skypiea-web/src/app/components/mail-box-staff/mail-box-staff.component.ts
@@ -13,6 +13,7 @@ import { nonMedicalObjectIcons } from 'src/app/util/nonMedicalObjectIcons.util';
 import { getNonMedicalObjectTypeName } from 'src/app/enums/nonMedicalObjectType.enum';
 import { notificationObjectInfo, NotificationObject } from 'src/app/enums/notificationObject.enum';
 import { MatExpansionPanel } from '@angular/material';
+import { State } from 'src/app/enums/state.enum';
 
 @Component({
   selector: 'app-mail-box-staff',
@@ -28,7 +29,7 @@ export class MailBoxStaffComponent implements OnInit {
   notificationObjectInfoArray: any[];
   typeName = (type) => getNonMedicalObjectTypeName(type);
   @ViewChild('mailsListComponent', { static: false }) mailsListComponent: MailsListComponent;
-  @ViewChild('concernedObjectExpansionPanel', { static: false}) concernedObjectExpansionPanel: MatExpansionPanel;
+  @ViewChild('concernedObjectExpansionPanel', { static: false }) concernedObjectExpansionPanel: MatExpansionPanel;
   roomAllOption: Room;
   occupiedRooms: Array<Room>;
   numberOfResidents: number;
@@ -36,6 +37,8 @@ export class MailBoxStaffComponent implements OnInit {
   notificationTemplates: Array<NotificationTemplate>;
   notificationToSend: Notification;
   selectedRoom: Room;
+  roomObjects: Array<any> = [];
+  roomFilteredObjects: Array<any> = [];
   concernedObject: any;
 
   constructor(private roomService: RoomService, private notificationService: NotificationService, private residentService: ResidentService, private toastService: ToastService) {
@@ -76,7 +79,7 @@ export class MailBoxStaffComponent implements OnInit {
       this.notificationToSend.messageBody = this.notificationToSend.messageBody.split('\n').join('<br>');
 
       if (this.selectedRoom.id != 0) {
-        if(this.notificationToSend.notificationObject == NotificationObject.ABOUT_OBJECT){
+        if (this.notificationToSend.notificationObject == NotificationObject.ABOUT_OBJECT) {
           const concernedObjectInfo: string = `Information about the concerned object :<br>Type: ${this.typeName(this.concernedObject.nonMedicalObjectType)}`;
           this.notificationToSend.messageBody = `${concernedObjectInfo}<br><br>${this.notificationToSend.messageBody}`;
         }
@@ -140,21 +143,62 @@ export class MailBoxStaffComponent implements OnInit {
     this.mailsListComponent.refreshMatTableData();
   }
 
+  /**
+   * Event which occurs when the user changes the selected room from the list
+   * @param event 
+   */
   onRoomSelectionChange(event) {
-    if (event && event.isUserInput) {
-      this.concernedObject = null;
+    this.concernedObject = null;
+    this.roomObjects = [];
+    if (this.selectedRoom && this.selectedRoom.id != this.roomAllOption.id) {
+      this.roomObjects = this.selectedRoom.nonMedicalConnectedObjects;
+      this.roomFilteredObjects = this.selectedRoom.nonMedicalConnectedObjects;
     }
+    //This method is called in order to display only the object according to the message Object (object state) when changing room
+    this.onNotificationTemplateChange(null)
+  }
+
+  onNotificationTemplateChange(event) {
+
+    if (this.notificationToSend.notificationObject && this.notificationToSend.notificationObject == NotificationObject.ABOUT_OBJECT) {
+      let object: string = this.notificationToSend.messageObject;
+      let enteredState: State = null;
+      if (object && object.trim().length > 0) {
+        object = object.toUpperCase();
+        //Search if the user entered a state in the notification object
+        if (object.indexOf(State.BROKEN) >= 0) {
+          enteredState = State.BROKEN;
+        } /*else if(object.indexOf(State.MISSING) >= 0){
+        enteredState = State.MISSING;
+        }*/ else if (object.indexOf(State.DANGER) >= 0) {
+          enteredState = State.DANGER;
+        } else if (object.indexOf(State.WARNING) >= 0) {
+          enteredState = State.WARNING;
+        }
+      }
+      if (enteredState == null) {
+        this.roomFilteredObjects = this.roomObjects;
+      } else {
+        this.roomFilteredObjects = this.roomObjects.filter((obj) => obj.state == enteredState);
+      }
+    }
+
+
+  }
+
+  clearMessageObject(){
+    this.notificationToSend.messageObject = "";
   }
 
   updateConcernedObject(object) {
     this.concernedObject = object;
   }
 
-  disableConcernedObjectExpansionPanel(){
-    const disable: boolean = (!this.selectedRoom || !this.selectedRoom.resident || this.selectedRoom.resident.id == 0) 
-    || (!this.notificationToSend || this.notificationToSend.notificationObject != NotificationObject.ABOUT_OBJECT);
+  disableConcernedObjectExpansionPanel() {
+    const disable: boolean = (!this.selectedRoom || !this.selectedRoom.resident || this.selectedRoom.resident.id == 0)
+      || (!this.notificationToSend || this.notificationToSend.notificationObject != NotificationObject.ABOUT_OBJECT);
 
-    if(disable && this.concernedObjectExpansionPanel){
+    if (disable && this.concernedObjectExpansionPanel) {
       this.concernedObjectExpansionPanel.close();
     }
 
